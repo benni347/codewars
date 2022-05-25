8c24c0daead8bf526631d985cd0aacea27026fe2
Cédric Skwar <cdrc@skwar.me>
2022-05-25 07:37:31 +0200

    Added 'Multiply'

---
+++ python/multiply/multiply/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Multiply
+:worth = 8 kyu
+:url   = https://www.codewars.com/kata/50654ddff44f800200000004
+:tags  = Bugs
+"""
+
+
+def multiply(a, b):
+    return a * b
---
+++ python/multiply/test.py
@@ -0,0 +1,13 @@
+import unittest
+import multiply
+
+
+class TestMultiply(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(multiply.multiply(2, 3), 6)
+        self.assertEqual(multiply.multiply(-10, -10), 100)
+        self.assertEqual(multiply.multiply(2, 0.5), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
