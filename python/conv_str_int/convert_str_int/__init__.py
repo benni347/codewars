3bffc64766e717c8bb31087eddc5b49086450524
Cédric Skwar <cdrc@skwar.me>
2022-05-25 11:30:22 +0200

    Added 'Convert str to int'

--- python/conv_str_int/convert_str_int/__init__.py
+++ python/conv_str_int/convert_str_int/__init__.py
@@ -4,3 +4,12 @@
 :url   = https://www.codewars.com/kata/544675c6f971f7399a000e79/
 :tags  = fundamentals, parsing, algorithms, strings
 """
+
+
+def string_to_number(s: str) -> int:
+    """
+    Convert a string to an int
+    :param s: The number as a string
+    :return: the number as an int
+    """
+    return int(s)
--- python/conv_str_int/test_convert.py
+++ python/conv_str_int/test_convert.py
@@ -1,9 +1,13 @@
 import unittest
+import convert_str_int
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestBasic(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(convert_str_int.string_to_number("1234"), 1234)
+        self.assertEqual(convert_str_int.string_to_number("605"), 605)
+        self.assertEqual(convert_str_int.string_to_number("1405"), 1405)
+        self.assertEqual(convert_str_int.string_to_number("-7"), -7)
 
 
 if __name__ == '__main__':
