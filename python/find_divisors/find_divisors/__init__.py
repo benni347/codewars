c39c13ea1139594be9deb15e609704cc1b1546be
Cédric Skwar <cdrc@skwar.me>
2022-05-25 07:33:12 +0200

    Added 'Find the divisors!'

---
+++ python/find_divisors/find_divisors/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Find the Divisors!
+:worth = 7 kyu
+:url   = https://www.codewars.com/kata/544aed4c4a30184e960010f4
+:tags  = Algorithms, mathematics, numbers
+"""
+
+
+def divisors(integer):
+    """
+    Find the divisors of an integer and if prime return integer is prime.
+    :param integer:
+    :return:
+    """
+    divisors_list = []
+    for i in range(1, integer):
+        if integer % i == 0:
+            divisors_list.append(i)
+    if len(divisors_list) == 1:
+        return f'{integer} is prime'
+    # remove 1 from divisors_list
+    divisors_list.remove(1)
+    return divisors_list
---
+++ python/find_divisors/test.py
@@ -0,0 +1,24 @@
+import unittest
+import find_divisors
+
+
+class TestDivisors(unittest.TestCase):
+    def test_single_divisor(self):
+        self.assertEqual(find_divisors.divisors(25), [5])
+
+    def test_two_divisors(self):
+        self.assertEqual(find_divisors.divisors(15), [3, 5])
+        self.assertEqual(find_divisors.divisors(253), [11, 23])
+
+    def test_12(self):
+        self.assertEqual(find_divisors.divisors(12), [2, 3, 4, 6])
+
+    def test_prime(self):
+        self.assertEqual(find_divisors.divisors(3), "3 is prime")
+        self.assertEqual(find_divisors.divisors(5), "5 is prime")
+        self.assertEqual(find_divisors.divisors(11), "11 is prime")
+        self.assertEqual(find_divisors.divisors(13), "13 is prime")
+
+
+if __name__ == '__main__':
+    unittest.main()
