c9948ae6d92e677452383121c63e6aff4570bda3
Cédric Skwar <cdrc+github@skwar.me>
2022-09-07 07:50:52 +0200

    Added: "Get Planet by ID" Probleme.

---
+++ python/planet_by_id/planet_by_id/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Get Planet by ID
+:worth =  8   kyu
+:url   =  https://www.codewars.com/kata/515e188a311df01cba000003/  
+:tags  =  Debugging
+"""
+
+
+def get_planet_name(planet_id):
+    # This doesn't work; Fix it!
+    if planet_id == 1:
+        return "Mercury"
+    if planet_id == 2:
+        return "Venus"
+    if planet_id == 3:
+        return "Earth"
+    if planet_id == 4:
+        return "Mars"
+    if planet_id == 5:
+        return "Jupiter"
+    if planet_id == 6:
+        return "Saturn"
+    if planet_id == 7:
+        return "Uranus"
+    if planet_id == 8:
+        return "Neptune"
---
+++ python/planet_by_id/test.py
@@ -0,0 +1,16 @@
+import unittest
+from planet_by_id import get_planet_name
+
+
+class MyTestCase(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(get_planet_name(2), 'Venus')
+        self.assertEqual(get_planet_name(5), 'Jupiter')
+        self.assertEqual(get_planet_name(3), 'Earth')
+        self.assertEqual(get_planet_name(4), 'Mars')
+        self.assertEqual(get_planet_name(8), 'Neptune')
+        self.assertEqual(get_planet_name(1), 'Mercury')
+
+
+if __name__ == '__main__':
+    unittest.main()
