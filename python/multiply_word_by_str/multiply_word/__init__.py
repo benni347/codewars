fca3f55f6bb36c8f0bb702e3af75e37bc2bcae0c
Cédric Skwar <cdrc@skwar.me>
2022-09-20 07:55:41 +0200

    Added the multiply word, by loc challange.

---
+++ python/multiply_word_by_str/multiply_word/__init__.py
@@ -0,0 +1,12 @@
+"""
+:URL: https://www.codewars.com/kata/5ace2d9f307eb29430000092/
+"""
+
+def modify_multiply(st, loc, num):
+    org_str: str = st
+    new_str: str = ""
+    mod_str: str = st.split()
+
+    new_str = (mod_str[loc] + "-") * num
+    return new_str[:-1]
+
