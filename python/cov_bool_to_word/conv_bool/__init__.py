11962a1dba3507fd9911fd2a74e5ed29e740230d
Cedric Skwar <cdrc@skwar.me>
2022-09-27 21:00:38 +0200

    Added convert bool to word probleme

---
+++ python/cov_bool_to_word/conv_bool/__init__.py
@@ -0,0 +1,8 @@
+"""
+:URL:   https://www.codewars.com/kata/53369039d7ab3ac506000467/
+"""
+
+def bool_to_word(boolean):
+    if boolean:
+        return "Yes"
+    return "No"
---
+++ python/cov_bool_to_word/test_conv_bool.py
@@ -0,0 +1,12 @@
+import unittest
+from conv_bool import bool_to_word
+
+
+class MyTestCase(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(bool_to_word(True), 'Yes')
+        self.assertEqual(bool_to_word(False), 'No')
+
+
+if __name__ == '__main__':
+    unittest.main()
