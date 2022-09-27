17c95d52d66fcb203f677f988ca917505d3e3296
Cedric Skwar <cdrc@skwar.me>
2022-09-27 20:56:20 +0200

    Added find uniq probleme

---
+++ python/find_unique/find_uniq/__init__.py
@@ -0,0 +1,25 @@
+"""
+:URL:   https://www.codewars.com/kata/585d7d5adb20cf33cb000235/
+"""
+
+
+def find_uniq(arr):
+    for i in range(len(arr)):
+        if i == 0:
+            if arr[i] != arr[i + 1] and arr[1] == arr[i + 2]:
+                return arr[i]
+        if i == len(arr) - 1:
+            return arr[i]
+        if i != 0:
+            if arr[i] != arr[-1] and arr[i] != arr[0]:
+                return arr[i]
+
+
+if __name__ == '__main__':
+    # find_uniq([3, 10, 3, 3, 3])
+    # find_uniq([3, 10, 3, 3, 3])
+    # unittest.main()
+    # mtc = MyTestCase()
+    # mtc.test_something()
+    r = find_uniq([0.8, 0.8, 0.8, 0.8, 0.11])
+    print(r)
---
+++ python/find_unique/test_uniq.py
@@ -0,0 +1,18 @@
+import unittest
+from find_uniq import find_uniq
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(find_uniq([1, 1, 1, 2, 1, 1]), 2)
+        self.assertEqual(find_uniq([0, 0, 0.55, 0, 0]), 0.55)
+        self.assertEqual(find_uniq([3, 10, 3, 3, 3]), 10)
+
+    def test_last_item(self):
+        self.assertEqual(find_uniq([3, 3, 3, 3, 10]), 10)
+        self.assertEqual(find_uniq([0.8, 0.8, 0.8, 0.8, 0.11]), 0.11)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
